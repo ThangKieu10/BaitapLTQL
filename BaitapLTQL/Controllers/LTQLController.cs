cba0a06b10f4a6df42c8085933ad40187668af3e
Admin <Admin@LAPTOP-2LGM8HPC>
2021-09-10 14:06:08 +0700

    1

---
+++ BaitapLTQL/Controllers/LTQLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaitapLTQL.Controllers
+{
+    public class LTQLController : Controller
+    {
+        // GET: LTQL
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
